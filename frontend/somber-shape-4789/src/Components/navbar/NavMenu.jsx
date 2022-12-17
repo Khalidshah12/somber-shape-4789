02851fe13d42e4712fa497dd51d7ace8115a00e2
Ashutosh Mahapatra <ashutoshm439@gmail.com>
2022-12-17 12:16:35 +0530

    ashu dya-5

--- frontend/somber-shape-4789/src/Components/navbar/NavMenu.jsx
+++ frontend/somber-shape-4789/src/Components/navbar/NavMenu.jsx
@@ -1,35 +1,48 @@
-import { Button, Flex, Menu, MenuButton } from "@chakra-ui/react";
+import { Button, Flex, Menu } from "@chakra-ui/react";
 import UserMenu from "./UserMenu";
 function NavMenu() {
   return (
     <>
       <Flex
-        // justify="center"
         justifyContent="space-around"
         gap={10}
         display={["none", "none", "flex", "flex"]}
         p={5}
       >
         <Menu>
-          <Button variant="ghost">BODY CARE</Button>
+          <Button color={"#004aad"} variant="ghost">
+            BODY CARE
+          </Button>
         </Menu>
         <Menu>
-          <Button variant="ghost">NEW & NOW</Button>
+          <Button color={"#004aad"} variant="ghost">
+            NEW & NOW
+          </Button>
         </Menu>
         <Menu>
-          <Button variant="ghost">HANDSOAP & SANITIZERS</Button>
+          <Button color={"#004aad"} variant="ghost">
+            HANDSOAP & SANITIZERS
+          </Button>
         </Menu>
         <Menu>
-          <Button variant="ghost">HOME FRAGRANCE</Button>
+          <Button color={"#004aad"} variant="ghost">
+            HOME FRAGRANCE
+          </Button>
         </Menu>
         <Menu>
-          <Button variant="ghost">GIFTS</Button>
+          <Button color={"#004aad"} variant="ghost">
+            GIFTS
+          </Button>
         </Menu>
         <Menu>
-          <Button variant="ghost">TOP OFFERS</Button>
+          <Button color={"#004aad"} variant="ghost">
+            TOP OFFERS
+          </Button>
         </Menu>
         <Menu>
-          <Button variant="ghost">GET INSPIRED</Button>
+          <Button color={"#004aad"} variant="ghost">
+            GET INSPIRED
+          </Button>
         </Menu>
         <UserMenu />
       </Flex>
