03057fa8f1296658c627944703e1e4fe39116851
Ashutosh Mahapatra <ashutoshm439@gmail.com>
2022-12-17 22:48:19 +0530

    ashu-day-5

--- frontend/somber-shape-4789/src/Components/navbar/NavMenu.jsx
+++ frontend/somber-shape-4789/src/Components/navbar/NavMenu.jsx
@@ -1,26 +1,82 @@
-import { Button, Flex, Menu, MenuButton } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Button, Flex, Menu } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 import UserMenu from "./UserMenu";
 function NavMenu() {
+  const navigate = useNavigate();
   return (
     <>
       <Flex
-        justify="center"
+        justifyContent="space-around"
         gap={10}
-        display={["none", "none", "flex", "flex"]}
+        display={["none", "none", "none", "flex"]}
         p={5}
       >
-         <Menu><Link>TOP OFFERS</Link></Menu>
-          <Menu><Link>CANDELS</Link></Menu>
-          <Menu><Link>HAND WASH</Link></Menu>
-          <Menu><Link>SHAMPOO</Link></Menu>
-          <Menu><Link>SHAMPOO</Link></Menu>
-          <Menu><Link>SHAMPOO</Link></Menu>
-          <Menu><Link>SHAMPOO</Link></Menu>
-          <Menu><Link>SANITIZERS</Link></Menu>
+        <Menu>
+          <Button
+            color={"#004aad"}
+            variant="ghost"
+            onClick={() => navigate("/")}
+          >
+            BODY CARE
+          </Button>
+        </Menu>
+        <Menu>
+          <Button
+            color={"#004aad"}
+            variant="ghost"
+            onClick={() => navigate("/")}
+          >
+            NEW & NOW
+          </Button>
+        </Menu>
+        <Menu>
+          <Button
+            color={"#004aad"}
+            variant="ghost"
+            onClick={() => navigate("/")}
+          >
+            HANDSOAP & SANITIZERS
+          </Button>
+        </Menu>
+        <Menu>
+          <Button
+            color={"#004aad"}
+            variant="ghost"
+            onClick={() => navigate("/")}
+          >
+            HOME FRAGRANCE
+          </Button>
+        </Menu>
+        <Menu>
+          <Button
+            color={"#004aad"}
+            variant="ghost"
+            onClick={() => navigate("/")}
+          >
+            GIFTS
+          </Button>
+        </Menu>
+        <Menu>
+          <Button
+            color={"#004aad"}
+            variant="ghost"
+            onClick={() => navigate("/")}
+          >
+            TOP OFFERS
+          </Button>
+        </Menu>
+        <Menu>
+          <Button
+            color={"#004aad"}
+            variant="ghost"
+            onClick={() => navigate("/")}
+          >
+            GET INSPIRED
+          </Button>
+        </Menu>
         <UserMenu />
       </Flex>
     </>
   );
 }
-  export default NavMenu;
+export default NavMenu;
