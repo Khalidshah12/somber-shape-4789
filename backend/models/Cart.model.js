4a96fee7a9e7f15968d4267968df7baf2e385ba8
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-03-25 02:54:49 +0530

    Update Cart.model.js

--- backend/models/Cart.model.js
+++ backend/models/Cart.model.js
@@ -4,7 +4,8 @@ const cartSchema = mongoose.Schema({
     product_count: { type: Number, default: 1 },
     product_id: { type: mongoose.Schema.Types.ObjectId, ref: 'product' },
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'auth' },
-}, {
+},
+{
     versionKey: false,
     timestamps: true
 });
